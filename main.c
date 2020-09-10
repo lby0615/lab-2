fa131ac96bc7a5f4ba1c6281d7ecdd2d097eb157
lby0615 <bjl5716@psu.edu>
2020-09-10 18:22:43 +0000

    format question

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 // Author: Boyuan Lai bjl5716@psu.edu
-// Collaborator: 
-// Collaborator:
-// Collaborator: 
+// Collaborator: Miranda Hanson mkh5823@psu.edu
+// Collaborator: Aaryan Darshin Bavishi aqb6298@psu.edu
+// Section: 6
+// Breakout: 11
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
@@ -12,28 +13,28 @@ if (grade >= 93.0 && grade <= 100.0) {
   printf("Your letter grade for CMPSC 131 is A.\n");
 }
 else if (grade >= 90.0 && grade < 93.0) {
-   printf("Your letter grade for CMPSC 131 is A-.\n");
+  printf("Your letter grade for CMPSC 131 is A-.\n");
 }
 else if (grade >= 87.0 && grade < 90.0) {
-   printf("Your letter grade for CMPSC 131 is B+.\n");
+  printf("Your letter grade for CMPSC 131 is B+.\n");
 }
 else if (grade >= 83.0 && grade < 87.0) {
-   printf("Your letter grade for CMPSC 131 is B.\n");
+  printf("Your letter grade for CMPSC 131 is B.\n");
 }
 else if (grade >= 80.0 && grade < 83.0) {
-   printf("Your letter grade for CMPSC 131 is B-.\n");
+  printf("Your letter grade for CMPSC 131 is B-.\n");
 }
 else if (grade >= 77.0 && grade < 80.0) {
-   printf("Your letter grade for CMPSC 131 is C+.\n");
+  printf("Your letter grade for CMPSC 131 is C+.\n");
 }
 else if (grade >= 70.0 && grade < 77.0) {
-   printf("Your letter grade for CMPSC 131 is C.\n");
+  printf("Your letter grade for CMPSC 131 is C.\n");
 }
 else if (grade >= 60.0 && grade < 70.0) {
-   printf("Your letter grade for CMPSC 131 is D.\n");
+  printf("Your letter grade for CMPSC 131 is D.\n");
 }
 else {
-   printf("Your letter grade for CMPSC 131 is F.\n");
+  printf("Your letter grade for CMPSC 131 is F.\n");
 }
   return 0;
 }
